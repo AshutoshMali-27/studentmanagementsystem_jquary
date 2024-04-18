aecd1073014b57791faacb8f6bb40f2cbaab6410
ashutosh <ashutoshmali67@gmail.com>
2024-04-18 23:33:27 +0530

    Add project files.

---
+++ studentmanagementsystemmvc/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using studentmanagementsystemmvc.Models;
+
+namespace studentmanagementsystemmvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7020/api/StudentAPI/";
+        // 
+
+        private HttpClient client = new HttpClient();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ClsStudent objstudent)
+        {
+            ClsStudent obj=new ClsStudent();
+            //obj
+
+            return Json("");
+        }
+    }
+}
---
+++ studentmanagementsystemmvc/Models/ClsStudent.cs
@@ -0,0 +1,13 @@
+﻿namespace studentmanagementsystemmvc.Models
+{
+    public class ClsStudent
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+    }
+}
